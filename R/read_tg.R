db20ded53d61f659d4f15301be46a5b8398b897a
Alistair Beith <alistair.beith@gmail.com>
2023-06-22 10:36:56 +0100

    Added encoding argument for read_tg()

--- R/read_tg.R
+++ R/read_tg.R
@@ -3,19 +3,20 @@
 #' Reads a Praat TextGrid as a nested tibble
 #'
 #' @param file path to TextGrid file
+#' @param encoding Passed to rPraat::tg.read: 'auto' (default) will detect encoding, or can be set to 'UTF-8' (rPraat default)
 #'
 #' @return Returns a nested tibble with `name`, `type` and `data`. `data` has the variables `t1`, `t2` and `label`
 #'
 #' @export
 
-read_tg <- function(file){
+read_tg <- function(file, encoding = 'auto'){
 
   name <- type <- NULL
 
   if(!grepl(".TextGrid$", file)) file <- paste0(file, ".TextGrid")
 
   # Read TextGrid
-  tg <- rPraat::tg.read(file)
+  tg <- rPraat::tg.read(file, encoding = 'auto')
 
   # Tidy
   tidy_grid <- tg %>%
