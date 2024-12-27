b565b6799b40a6b17f8f548bfa6eac2f5bf2376a
Alistair Beith <alistair.beith@gmail.com>
2024-12-27 19:00:10 +0000

    Added Wave import from tuneR

--- R/data.R
+++ R/data.R
@@ -5,4 +5,5 @@
 #' @format A tuneR "Wave" object:
 #'
 #' @references Tilsen, S., & Johnson, K. (2008). Low-frequency Fourier analysis of speech rhythm. The Journal of the Acoustical Society of America, 124(2), EL34–EL39. doi:10.1121/1.2947626
+#' @importMethodsFrom tuneR Wave
 "mm1"
